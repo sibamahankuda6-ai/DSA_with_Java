da7c8603eb80a4b8ebd9c27ead3bddd31335ab6c
Siba mahankuda <sibamahankuda6@gmail.com>
2026-08-26 22:13:47 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -16,6 +16,7 @@ public class Palindrome {
         System.out.println("not a pallindrome number");
        }
     }
+    
      public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a number :");
